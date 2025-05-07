459866bf966c52405d4978fb4559a5c6e026d681
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-05-07 16:20:40 +0600

    changed nothing

--- debugging.cpp
+++ debugging.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int i = 10, j = 5;
-    int modResult = 0;
-    int divResult = 0;
-
-    modResult = i % j;
-    cout << modResult << " ";
-
-    divResult = i / modResult;
-    cout << divResult;
-
+int main() {
+    
     return 0;
 }
