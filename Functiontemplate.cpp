277e2effa6536a900261ef458d93639be6a2b23b
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-05-14 12:02:36 +0600

    initialized

--- Functiontemplate.cpp
+++ Functiontemplate.cpp
@@ -1,3 +1,22 @@
 //
 // Created by Shahidur Rahman Shoruv on 9/5/25.
 //
+#include <iostream>
+using namespace std;
+
+#define nl << endl
+
+template <class T>
+
+void swapIng(T &x, T &y) {
+    T temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+    swapIng(x,y);
+    cout << x << y nl;
+}
--- debugging.cpp
+++ debugging.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class student
+{
+    public:
+    string name;
+    string dept_name;
+    int roll;
+    int b;
+    float x;
+    float y;
+
+};
+
+
+
 int main() {
-    
+
+    student s1,s2;
+    s1.name = "Mahnudul";
+    s1.roll = 240106;
+
+    s2.name = "Arnob";
+    s2.roll = 240123;
+
+    cout << s1.name << endl;
     return 0;
 }
